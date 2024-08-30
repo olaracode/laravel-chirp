c841a2131dee407db8212cfe43b5f5bd4ac4b9ee
Octavio Lara <octavioasapchi@gmail.com>
2024-08-29 23:50:22 -0300

    ✨  ADD chirp delete

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -78,6 +78,9 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+        return redirect(route('chirps.index'));
     }
 }
