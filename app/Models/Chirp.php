214e03ad2322fe5df6d60fcfc13e0be17c6332cb
Octavio Lara <octavioasapchi@gmail.com>
2024-08-30 00:04:54 -0300

    ✨ ADD notifications and mail setup

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
@@ -12,6 +12,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
