2355d49f4ee72f18c53660ee157e1b50b476665b
Octavio Lara <octavioasapchi@gmail.com>
2024-08-30 19:28:35 -0300

    🚧 ADD new post functionality

--- resources/js/Components/Chirp/New.tsx
+++ resources/js/Components/Chirp/New.tsx
@@ -1,17 +1,33 @@
-import React from "react";
-
+import React, { FormEventHandler } from "react";
+import { useForm, usePage } from "@inertiajs/react";
+import InputError from "@/Components/InputError";
 const New = () => {
+    const { data, setData, post, processing, reset, errors } = useForm({
+        message: "",
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+        post(route("chirps.store"), { onSuccess: () => reset() });
+    };
     return (
-        <div className="p-4 border-b border-gray-200">
+        <form onSubmit={submit} className="p-4 border-b border-gray-200">
             <textarea
+                value={data.message}
                 className="w-full p-2 rounded-lg border border-gray-300"
                 placeholder="What's happening?"
                 rows={3}
+                onChange={(e) => setData("message", e.target.value)}
             ></textarea>
-            <button className="bg-blue-400 text-white rounded-full px-4 py-2 mt-2 hover:bg-blue-500">
+            <InputError message={errors.message} className="mt-2" />
+            <button
+                type="submit"
+                disabled={processing}
+                className="bg-blue-400 text-white rounded-full px-4 py-2 mt-2 hover:bg-blue-500"
+            >
                 Tweet
             </button>
-        </div>
+        </form>
     );
 };
 
